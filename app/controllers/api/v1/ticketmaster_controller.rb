344356a6137e02f8e36f0d022c2507e7e498c54c
foymikek <foymikek@gmail.com>
2021-06-06 15:03:21 -0600

    Add refactored ticket master API consumptiona and exposure.

--- app/controllers/api/v1/ticketmaster_controller.rb
+++ app/controllers/api/v1/ticketmaster_controller.rb
@@ -3,18 +3,8 @@ class Api::V1::TicketmasterController < ApplicationController
     keyword_query = "Red Rocks"
     city_query    = "Morrison"
 
-    conn = Faraday.new(url: "https://app.ticketmaster.com/") do |faraday|
-      faraday.params[:apikey] = ENV['ticket_master_api_key']
-    end
+    event_poros   = TicketMasterFacade.event_search(keyword_query, city_query)
 
-    response = conn.get("discovery/v2/events.json?keyword=#{keyword_query}&city=#{city_query}")
-    
-    data  = JSON.parse(response.body, symbolize_names: true)
-    
-    event_poros = data[:_embedded][:events][0..4].map do |event_data|
-      TicketMaster.new(event_data)
-    end
-    
     render json: EventSerializer.new(event_poros)
   end
 end
--- app/services/ticket_master_service.rb
+++ app/services/ticket_master_service.rb
@@ -1,2 +1,12 @@
-class TicketMasterService
+class TicketMasterService < BaseService
+  class << self
+    def event_search_db(keyword_query, city_query)
+      response = conn("https://app.ticketmaster.com/").get("discovery/v2/events.json") do |f|
+        f.params[:apikey]  = ENV['ticket_master_api_key']
+        f.params[:keyword] = keyword_query
+        f.params[:city]    = city_query
+      end
+      format_json(response)[:_embedded][:events][0..4]
+    end
+  end
 end
