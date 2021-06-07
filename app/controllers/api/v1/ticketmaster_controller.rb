b6edccbc52336f2afd2a62078408cfb54d108f7e
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-06 19:45:37 -0600

    Remove commented code

--- app/controllers/api/v1/ticketmaster_controller.rb
+++ app/controllers/api/v1/ticketmaster_controller.rb
@@ -1,12 +1,7 @@
 class Api::V1::TicketmasterController < ApplicationController
   def index
-    # keyword_query = "Red Rocks"
-    # city_query    = "Morrison"
-
     event_poros = TicketMasterFacade.event_search(params[:keyword], params[:city])
 
     render json: EventSerializer.new(event_poros)
-    # render json: TicketMasterFacade.event_search(params[:keyword], params[:city])
-
   end
 end
