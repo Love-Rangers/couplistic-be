b1b42d5b850a6e9d5bbf760ce5a5871bb0064e59
r-stemmle <ryan.stemmle@gmail.com>
2021-06-08 18:45:07 -0400

    Pass happy path for events create

---
+++ spec/requests/events/events_post_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'POST api/v1/events' do
+  it "happy path: it can create an event record" do
+    params = {
+      "location"=>"red rocks",
+      "name"=>"Jax",
+      "start_time"=>"2021-06-11T20:28",
+      "user_id"=>"2"
+    }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/events', headers: headers, params: JSON.generate(params)
+    created_event = Event.last
+    expect(response).to be_successful
+    expect(created_event.location).to eq("red rocks")
+    expect(created_event.name).to eq("Jax")
+    expect(created_event.start_time).to eq('Fri, 11 Jun 2021 20:28:00 UTC +00:00')
+    expect(created_event.user_id).to eq(2)
+  end
+end
