2391aff43b37bd13f8a5ead72705c10428b10f16
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-10 09:56:51 -0600

    remove factory bot issues

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :random_event, class: Event do
     name { Faker::Book.title }
-    start_time { Faker::Date.in_date_period(month: 6) }
+    start_time { Faker::Date.between_except(from: 1.year.ago, to: 1.year.from_now, excepted: Date.today) }
     location { Faker::Restaurant.name }
     user_id { Faker::Number.number(digits: 3) }
   end
